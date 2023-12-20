61477bcd9c65ba2a284b8b69ff15bf81d31435ca
Shidhanta95 <147026855+Shidhanta95@users.noreply.github.com>
2023-12-20 17:33:14 +0530

    Add files via upload

---
+++ datavisualization.py
@@ -0,0 +1,43 @@
+from extract_data import extract_data
+from PIL import Image
+import requests
+from io import BytesIO
+import zipfile
+import pathlib 
+import random
+import os
+
+def visualise_image():
+    url = extract_data()
+    url_response = requests.get(url)
+    with zipfile.ZipFile(BytesIO(url_response.content)) as z:
+        z.extractall('.')
+    buildings = os.listdir(os.path.join(os.getcwd(),"seg_train/seg_train/buildings"))
+    forest = os.listdir(os.path.join(os.getcwd(),"seg_train/seg_train/forest"))
+    glacier = os.listdir(os.path.join(os.getcwd(),"seg_train/seg_train/glacier"))
+    mountain = os.listdir(os.path.join(os.getcwd(),"seg_train/seg_train/mountain"))
+    sea = os.listdir(os.path.join(os.getcwd(),"seg_train/seg_train/sea"))
+    street = os.listdir(os.path.join(os.getcwd(),"seg_train/seg_train/street"))
+    path = pathlib.Path(os.path.join(os.getcwd(),"seg_train/seg_train"))
+    def open_random_image(path):
+        # Get a list of all files in the folder
+        all_files = os.listdir(path)
+        random_image_file = random.choice(all_files)
+        image_path = os.path.join(path, random_image_file)
+        image = Image.open(image_path)
+        return image
+    buildings_image = open_random_image(os.path.join(os.getcwd(),"seg_train/seg_train/buildings"))
+    forest_image = open_random_image(os.path.join(os.getcwd(),"seg_train/seg_train/forest"))
+    glacier_image = open_random_image(os.path.join(os.getcwd(),"seg_train/seg_train/glacier"))
+    mountain_image = open_random_image(os.path.join(os.getcwd(),"seg_train/seg_train/mountain"))
+    sea_image = open_random_image(os.path.join(os.getcwd(),"seg_train/seg_train/sea"))
+    street_image = open_random_image(os.path.join(os.getcwd(),"seg_train/seg_train/street"))
+    buildings_image.save('building_image.jpg')
+    forest_image.save('forest_image.jpg')
+    glacier_image.save('glacier_image.jpg')
+    mountain_image.save('mountain_image.jpg')
+    sea_image.save('sea_image.jpg')
+    street_image.save('street_image.jpg')
+    return path,glioma_tumor_images,meningioma_tumor_images,no_tumor_images,pituitory_tumor_images
+
+visualise_image()
